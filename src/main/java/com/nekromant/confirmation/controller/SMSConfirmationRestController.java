26842a73839938d0a2b3eef14ed3c8be4d947cfe
CeJoW <32615273+CeJoW@users.noreply.github.com>
2021-05-21 17:45:47 +0300

    Добавлена работа с токенами для email подтверждения

--- src/main/java/com/nekromant/confirmation/controller/SMSConfirmationRestController.java
+++ src/main/java/com/nekromant/confirmation/controller/SMSConfirmationRestController.java
@@ -3,6 +3,7 @@ package com.nekromant.confirmation.controller;
 import com.nekromant.confirmation.service.SMSCodeService;
 import dto.SMSCodeDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,9 +13,13 @@ public class SMSConfirmationRestController {
     @Autowired
     private SMSCodeService smsCodeService;
 
+    @Value("${app.const.smsCodeExpiredDelayInSeconds}")
+    private String codeExpiredDelay;
+
     @PostMapping("create")
-    public void createSMSCode(@RequestBody SMSCodeDTO smsCodeDTO) {
+    public Long createSMSCode(@RequestBody SMSCodeDTO smsCodeDTO) {
         smsCodeService.createCode(smsCodeDTO.getPhoneNumber());
+        return Long.valueOf(codeExpiredDelay);
     }
 
     @PostMapping("verify")
